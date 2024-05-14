9495b1104d2a0da85d3ea07f947a4244e2b0947a
Salah <salah.abdelhakim@yh.nackademin.se>
2024-05-14 21:07:52 +0200

    s

--- pyhtoncreateprofiles-main/pyhtoncreateprofiles-main/csvtojson.py
+++ pyhtoncreateprofiles-main/pyhtoncreateprofiles-main/csvtojson.py
@@ -1,20 +1,18 @@
-import csv
-import json
+import csv #att importera och arbeta med CSV filen 
+import json #att importera och arbeta med json filen
 
-def csv_to_json(csv_file, json_file):
-    # Open the CSV file and read its contents
-    with open(csv_file, 'r') as csvfile:
-        # Parse the CSV file
-        csvreader = csv.DictReader(csvfile)
-        
-        # Convert CSV data to a list of dictionaries
-        data = []
-        for row in csvreader:
-            data.append(row)
+#skapa en funktion (convertera CSV till Json)
+def csv_till_json(csv_fil, json_fil):
+    data = []
+    with open(csv_fil, 'r', encoding='utf-8-sig') as csvfil:  # utf-8-sig. det är för svenska bokstäverna (å, ä och ö)
+        läsare = csv.DictReader(csvfil) #den läser innehållet från csv filen.
+        for rad in läsare:
+            data.append(rad)
     
-    # Write the JSON data to a file
-    with open(json_file, 'w') as jsonfile:
-        json.dump(data, jsonfile, indent=4)
-
-# Example usage
-csv_to_json('profiles1.csv', 'nydata.json')
+    with open(json_fil, 'w', encoding='utf-8') as jsonfil:  # det är för svenska bokstäverna (å, ä och ö)
+        json.dump(data, jsonfil, indent=4, ensure_ascii=False)  
+#skapa if-sats
+if __name__ == "__main__":
+    csv_fil = "profiles1.csv"
+    json_fil = "data.json"
+    csv_till_json(csv_fil, json_fil) #printa resultat. 
